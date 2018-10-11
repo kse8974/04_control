c355455f1bfca99c619c6511fba6ba7aba715764
kse8974 <kse8974@naver.com>
2018-10-11 16:15:22 +0900

    04.control.practice5

--- main.c
+++ main.c
@@ -4,28 +4,25 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	char x;
+	char str[100];
+	int num = 0;
+	char c;
+
 	
-	printf("input a char : ");
-	scanf("%c", &x);
+	printf("input a string : ");
 	
-	if('A'<=x && x<='Z')
+	
+	while ((c=getchar()) != '\n')
 	{
-	printf("이것은 대문자 입니다.\n");
-	}
 		
-	else if('a'<=x && x<'z')
+	if('0'<=c && c<='9')
 	{
-	printf("이것은 소문자 입니다.\n");
+	 num++;
 	}
 	
-	else if('0'<= x && x<='9')
-	{
-		printf("이것은 숫자 입니다.\n");
 	}
-	else
-	{
-		printf("이것은 기타글자  입니다.\n");
-	}
+	
+	printf("The number of digits is %d", num);
+	
 	return 0;
 }
